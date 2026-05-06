c00030da13001aca57c55094adf70b741adfc1bd
lcqh2635 <lcqh2635@gmail.com>
2026-05-06 15:44:53 +0800

    feat: improve some file

---
+++ services/auth-service/src/test/java/com/mallcloud/service/auth/AuthApplicationTest.java
@@ -0,0 +1,14 @@
+package com.mallcloud.service.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AuthApplicationTest {
+
+    @Test
+    void demo() {
+        System.out.println("test");
+    }
+
+}
