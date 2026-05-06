f33b29207198ff41ff27ee2215c42ee1f2fc0fe0
lcqh2635 <lcqh2635@gmail.com>
2026-05-06 11:55:22 +0800

    feat: init mall-cloud project structure

---
+++ src/test/java/com/mall/cloud/MallCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mall.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
